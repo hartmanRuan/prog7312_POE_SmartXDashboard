4ba044e7b8b704a5f35f4a050e6e2d68540acb3f
Ruan Hartman <st10453370@imconnect.edu.za>
2026-08-27 14:52:10 +0200

    Updated MainWindow.xaml.cs
    Updated the dashboard/landing page. Using RefreshDashboardMetics method for realtime dashboard stats. Made use of AnalyticalService.

--- SmartXDashboard/SmartXDashboard/MainWindow.xaml.cs
+++ SmartXDashboard/SmartXDashboard/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SmartXDashboard.Models;
+using SmartXDashboard.Services;
 
 namespace SmartXDashboard
 {
@@ -16,12 +12,16 @@ namespace SmartXDashboard
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly AnalyticsService _analyticsService = new AnalyticsService();
+
         public MainWindow()
         {
             InitializeComponent();
 
             MainContentFrame.Children.Clear();
             MainContentFrame.Children.Add(new SensorIngestionView());
+
+            RefreshDashboardMetrics();
         }
 
         private void NavProvisioning_Click(object sender, RoutedEventArgs e)
@@ -41,6 +41,41 @@ namespace SmartXDashboard
             LoginWindow login = new LoginWindow();
             login.Show();
             this.Close();
+        }
+
+        /// <summary>
+        /// Recalculates metrics using AnalyticsService and updates header/summary card controls.
+        /// </summary>
+        public void RefreshDashboardMetrics(IEnumerable<TelemetryPacket<double>> livePackets = null)
+        {
+            var samplePackets = livePackets ?? GetInitialSamplePackets();
+            var metrics = _analyticsService.CalculateMetrics(samplePackets);
+
+            // Safely assign metrics if header or summary text controls exist in MainWindow.xaml
+            if (FindName("TotalNodesText") is TextBlock totalNodesText)
+                totalNodesText.Text = metrics.ActiveNodesCount.ToString();
+
+            if (FindName("TotalPacketsText") is TextBlock totalPacketsText)
+                totalPacketsText.Text = metrics.TotalPacketsReceived.ToString();
+
+            if (FindName("AvgValueText") is TextBlock avgValueText)
+                avgValueText.Text = $"{metrics.AveragePayloadValue:F1}";
+
+            if (FindName("WarningAlertsText") is TextBlock warningAlertsText)
+                warningAlertsText.Text = metrics.WarningAlertsCount.ToString();
+        }
+
+        private List<TelemetryPacket<double>> GetInitialSamplePackets()
+        {
+            var nodes = SensorRepository.Instance.GetAllNodes().ToList();
+            var list = new List<TelemetryPacket<double>>();
+
+            foreach (var node in nodes)
+            {
+                list.Add(new TelemetryPacket<double>(node.MacAddress, node.LocationZone, 22.4, "°C", NodeStatus.Active));
+            }
+
+            return list;
         }
     }
 }
